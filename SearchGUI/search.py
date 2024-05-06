759597cc9bbdf583b90ba78bfaa3b069a0cf29af
= <joar.gunnarsson@gmail.com>
2024-05-06 19:27:20 +0200

    Fixed audio loading, removed unused functions

--- SearchGUI/search.py
+++ SearchGUI/search.py
@@ -42,40 +42,6 @@ class QueryType:
     new = "new"
 
 
-def index_documents_from_folder(folder_path, index_name):
-    for root, dirs, files in os.walk(folder_path):
-        for file_name in files:
-            print(file_name)
-            if file_name.endswith('.json'):
-                file_path = os.path.join(root, file_name)
-                with open(file_path, 'r', encoding='utf-8') as file:
-                    document = json.load(file)
-                    # Index the document into Elasticsearch
-                    client.index(index=index_name, body=document)
-
-
-def index_transcripts_from_folder(folder_path, index_name):
-    number_of_files = 0
-    for root, dirs, files in os.walk(folder_path):
-        for file_name in files:
-            if number_of_files % 100 == 1:
-                print(f"{number_of_files} files indexed.")
-            if file_name.endswith('.json'):
-                file_path = os.path.join(root, file_name)
-                with open(file_path, 'r', encoding='utf-8') as file:
-                    document = json.load(file)
-                    alternatives = document["results"]
-                    for alt in alternatives:
-                        try:
-                            transcript = alt["alternatives"][0]["transcript"]
-                            contents = {"transcript": transcript}
-                            client.index(index=index_name, body=contents)
-                        except KeyError:
-                            pass
-                    # Index the document into Elasticsearch
-            number_of_files += 1
-
-
 def get_transcript_metadata(results, ids):
     if len(results) == 0:
         return []
@@ -332,43 +298,6 @@ def check_spelling(query_string):
 
 def execute_query(query, n=10, index=INDEX):
     return client.search(index=index, body=query, size=n, source=["show_id", "transcript", "starttime", "endtime"])
-
-
-def delete_index(index_name):
-    # Check if the index exists
-    if client.indices.exists(index=index_name):
-        # Delete the index
-        client.indices.delete(index=index_name)
-        print(f"Index '{index_name}' deleted successfully.")
-    else:
-        print(f"Index '{index_name}' does not exist.")
-
-
-def create_index(index_name):
-    # Define index settings and mappings if needed
-    # You can define your index settings and mappings here
-    # For simplicity, we'll skip this step
-
-    # Create the index
-    client.indices.create(index=index_name)
-    print(f"Index '{index_name}' created successfully.")
-
-
-def display_number_of_hits(response):
-    print("Number of results:", response['hits']['total']['value'])
-
-
-def get_number_of_hits(response):
-    return response['hits']['total']['value']
-
-
-def print_first_n_results(response, n=10):
-    for hit in response['hits']['hits'][:n]:
-        transcript = hit['_source']["transcript"]
-        print("-" * 30)
-        print("Score:", hit["_score"])
-        print("Transcript: " + transcript)
-        print("-" * 30 + "\n")
 
 
 def get_first_n_results(response, n=10):
